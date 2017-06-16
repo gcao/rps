15959a8a1d79dc12de4c94d1c82f0b4827a2ebcc
Guoliang Cao <gcao99@gmail.com>
2017-06-16 18:47:53 -0400

    Clean up home component handlers

--- src/components/home/handlers.js
+++ src/components/home/handlers.js
@@ -10,63 +10,67 @@ export const STATE_KEY = 'home'
 
 const ACTION_DETECTION_INTERVAL = 150
 
+let actionDetector
+
+function startTimer(action, store) {
+  setTimeout(() => {
+    let { started, videoPaused } = store.getState()[STATE_KEY]
+    if (!started || videoPaused) {
+      return
+    }
+
+    let video  = document.querySelector('video')
+    let canvas = document.querySelector('canvas')
+    if (video && canvas) {
+      let image = capture(video, canvas)
+      store.dispatch(actions.detect(image))
+    }
+  }, ACTION_DETECTION_INTERVAL)
+}
+
+export function start(action, {store}) {
+  let imageClassifier = getImageClassifier()
+  actionDetector = new ActionDetector(imageClassifier)
+
+  startTimer(action, store)
+}
+
+export function resume(action, {store}) {
+  startTimer(action, store)
+}
+
+export function detect(action, {store}) {
+  let image  = action.payload
+  let result = actionDetector.detect(image)
+  if (result.detected) {
+    // let the AI predict and play, then train with the real human move, add to game state
+    let computerPlayer = getComputerPlayer()
+    let { rounds }     = store.getState()[STATE_KEY]
+    let gameState      = new GameState(rounds)
+    let prediction     = computerPlayer.predict(gameState)
+
+    let humanMove    = result.imageClass
+    let computerMove = prediction.winningMove
+
+    computerPlayer.train(gameState, humanMove)
+
+    setTimeout(() => store.dispatch(actions.play({
+      player1Move: humanMove,
+      player2Move: computerMove,
+    })), 0)
+
+    setTimeout(() => startTimer(action, store), 1500)
+  } else {
+    startTimer(action, store)
+  }
+}
+
 let handlers = []
 
 export function registerHandlers() {
-  let actionDetector
-
-  handlers.push(addHandler(actions.START, (action, {store}) => {
-    let imageClassifier = getImageClassifier()
-    actionDetector = new ActionDetector(imageClassifier)
-
-    startTimer(action, store)
-  }))
-
-  handlers.push(addHandler(actions.RESUME, (action, {store}) => {
-    startTimer(action, store)
-  }))
-
-  let startTimer = (action, store) => {
-    setTimeout(() => {
-      let { started, videoPaused } = store.getState()[STATE_KEY]
-      if (!started || videoPaused) {
-        return
-      }
-
-      let video  = document.querySelector('video')
-      let canvas = document.querySelector('canvas')
-      if (video && canvas) {
-        let image = capture(video, canvas)
-        store.dispatch(actions.detect(image))
-      }
-    }, ACTION_DETECTION_INTERVAL)
-  }
-
-  handlers.push(addHandler(actions.DETECT, (action, {store}) => {
-    let image  = action.payload
-    let result = actionDetector.detect(image)
-    if (result.detected) {
-      // let the AI predict and play, then train with the real human move, add to game state
-      let computerPlayer = getComputerPlayer()
-      let { rounds }     = store.getState()[STATE_KEY]
-      let gameState      = new GameState(rounds)
-      let prediction     = computerPlayer.predict(gameState)
-
-      let humanMove    = result.imageClass
-      let computerMove = prediction.winningMove
-
-      computerPlayer.train(gameState, humanMove)
-
-      setTimeout(() => store.dispatch(actions.play({
-        player1Move: humanMove,
-        player2Move: computerMove,
-      })), 0)
-
-      setTimeout(() => startTimer(action, store), 1500)
-    } else {
-      startTimer(action, store)
-    }
-  }))
+  handlers.push(addHandler(actions.START, start))
+  handlers.push(addHandler(actions.RESUME, resume))
+  handlers.push(addHandler(actions.DETECT, detect))
 }
 
 export function deregisterHandlers() {
