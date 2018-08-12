8e41159f38b4ed2cce0296325ad1a93dd5fe465f
Guoliang Cao <gcao99@gmail.com>
2018-08-11 22:49:25 -0400

    Simplify reducers

--- src/components/computer-player/reducers.ts
+++ src/components/computer-player/reducers.ts
@@ -1,35 +1,34 @@
-import { addReducer } from '../../reducers'
-import replace from '../../reducers/replace'
-import update from '../../reducers/update'
+import { addReducerFor, replace, update } from '../../reducers'
 import { INITIALIZE, PLAY } from './actions'
 import Round from '../../rps/Round'
 import Action from '../../common/Action'
 
 export const STATE_KEY = 'computerPlayer'
 
-export default function reducers(state: any, action: Action) {
-  switch (action.type) {
-    case INITIALIZE:
-      return replace(state, STATE_KEY, {
-        initialized: true,
-        rounds: [],
-        implementation: action.payload,
-      })
-
-    case PLAY:
-      let { rounds } = state[STATE_KEY]
-      let { player1Move, player2Move, prediction } = action.payload
-
-      rounds = rounds.concat(new Round(player1Move, player2Move))
-
-      return update(state, STATE_KEY, {
-        rounds,
-        prediction
-      })
-
-    default:
-      return state
-  }
+export function initialize(state: any, action: Action) {
+  return replace(state, STATE_KEY, {
+    initialized: true,
+    rounds: [],
+    implementation: action.payload,
+  })
 }
 
-addReducer(reducers)
+export function play(state: any, action: Action) {
+  let { rounds } = state[STATE_KEY]
+  let { player1Move, player2Move, prediction } = action.payload
+
+  rounds = rounds.concat(new Round(player1Move, player2Move))
+
+  return update(state, STATE_KEY, {
+    rounds,
+    prediction
+  })
+}
+
+//addReducerFor(PLAY,       play)
+//addReducerFor(INITIALIZE, initialize)
+
+addReducerFor({
+  [PLAY]:       play,
+  [INITIALIZE]: initializeie,
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,9 @@
 import ReducerGroup from './ReducerGroup'
+import Action  from './common/Action'
+import replace from './replace'
+import update  from './update'
+
+export { replace, update }
 
 const defaultReducers = new ReducerGroup()
 
@@ -8,6 +13,28 @@ export function addReducer(reducer) {
 
 export function removeReducer(reducer) {
   defaultReducers.remove(reducer)
+}
+
+//export function addReducerFor(actionType, reducer) {
+//  defaultReducers.add(function(state: any, action: Action) {
+//    if (action.type === actionType) {
+//      return reducer(state, action)
+//    } else {
+//      return state
+//    }
+//  })
+//}
+
+export function addReducerFor(mappings) {
+  defaultReducers.add(function(state: any, action: Action) {
+    for (var key in mappings) {
+      var reducer = mappings[key]
+      if (action.type === key) {
+        return reducer(state, key)
+      }
+    }
+    return state
+  })
 }
 
 export default function(state, action) {
