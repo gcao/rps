12faaa44ba4e19e185056835a87ab6510aedd869
Guoliang Cao <gcao99@gmail.com>
2024-07-23 17:13:30 -0400

    Clean up ComputerPlayerSwitcher

--- src/common/computer-player.ts
+++ src/common/computer-player.ts
@@ -1,12 +1,19 @@
+/*global localStorage */
+
 import IComputerPlayer from '../rps/computer-player/IComputerPlayer'
-// import MainPlayer from '../rps/computer-player/MainPlayer'
 import DqnPlayer from '../rps/computer-player/DqnPlayer'
+import DummyPlayer from '../rps/computer-player/DummyPlayer'
 
 let computerPlayer: IComputerPlayer
 
 export function setComputerPlayer() {
-  // computerPlayer = new MainPlayer()
-  computerPlayer = new DqnPlayer()
+  let dummyPlayer = new DummyPlayer()
+
+  if (localStorage.getItem('opponent') === dummyPlayer.name) {
+    computerPlayer = dummyPlayer
+  } else {
+    computerPlayer = new DqnPlayer()
+  }
 }
 
 export function getComputerPlayer(): IComputerPlayer {
