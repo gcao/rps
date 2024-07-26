b6309555d4cf6792a264a671949ae3420992adc7
Guoliang Cao <gcao99@gmail.com>
2024-07-26 11:20:35 -0400

    Add logo etc

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,9 +7,9 @@ export default class Homepage extends Component {
   render() {
     return (
       <div>
-        <img src="/path/to/logo.png" alt="Logo" style={{ width: '100px', height: '100px' }} />
-        <h1>Welcome to Our Application</h1>
-        
+        <img src="/images/rps-logo.png" alt="Logo" style={{ width: '100px', height: '100px' }} />
+        <h1>Welcome to Rock Paper Scissors: YOU vs THE MACHINE</h1>
+
         <p>
           If you haven't, <Link to='/action-trainer'>train the gesture detector</Link> first.
         </p>
