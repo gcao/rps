6d46d7e216ed44de3a96a1554fec061d2ea6dd44
Guoliang Cao <gcao99@gmail.com>
2024-07-26 14:30:17 -0400

    Fix html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,27 +6,29 @@ import ComputerPlayerSwitcher from '../components/ComputerPlayerSwitcher'
 export default class Homepage extends Component {
   render() {
     return (
-      <div className="header">
-        <img src="/images/rps-logo.png" alt="Logo" className="logo" />
-        <h1>Welcome to Rock Paper Scissors: YOU vs THE MACHINE</h1>
-      </div>
       <div>
+        <div className="header">
+          <img src="/images/rps-logo.png" alt="Logo" className="logo" />
+          <h1>Welcome to Rock Paper Scissors: YOU vs THE MACHINE</h1>
+        </div>
+        <div>
 
-        <p>
-          If you haven't, <Link to='/action-trainer'>train the gesture detector</Link> first.
-        </p>
+          <p>
+            If you haven't, <Link to='/action-trainer'>train the gesture detector</Link> first.
+          </p>
 
-        <p>
-          <ComputerPlayerSwitcher/>
-        </p>
+          <p>
+            <ComputerPlayerSwitcher />
+          </p>
 
-        <p>
-          <Home/>
-        </p>
+          <p>
+            <Home />
+          </p>
 
-        <p>
-          You can also <Link to='/computer-player'>play with the machine without camera</Link>.
-        </p>
+          <p>
+            You can also <Link to='/computer-player'>play with the machine without camera</Link>.
+          </p>
+        </div>
       </div>
     )
   }
